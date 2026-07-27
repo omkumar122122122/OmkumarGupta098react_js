338e4c52b47816673450656e860ba531c452ab07
omkumar122122122 <omkumar122122122@gmail.com>
2026-07-27 16:34:19 +0530

    all done

--- Omkumargupta2024BTCS098/src/features/contact/Contact.jsx
+++ Omkumargupta2024BTCS098/src/features/contact/Contact.jsx
@@ -3,13 +3,14 @@ import React, { useState } from 'react'
 import Nav from '../../shared/components/Nav'
 
 const Contact = ({detail}) => {
-    const [name,setname]=usestate("")
-    const [email,setemai]=usestate("")
-    const [pno,setpno]=usestate("")
-    const [show,setshow]=usestate(false)
+    const [name,setname]=useState("")
+    const [email,setemai]=useState("")
+    const [pno,setpno]=useState("")
+    const [show,setshow]=useState(false)
     const [pname,setpname]=useState("")
 
     function handelclick(e){
+        e.preventDefault();
         setpname(name);
 
         setemai("")
@@ -30,9 +31,9 @@ const Contact = ({detail}) => {
             <input type="text" value={email} placeholder='enter your email' onChange={(e)=>setemai(e.target.value)} />
             <input type="text" value={pno}  placeholder='enter your phone number' onChange={(e)=>setpno(e.target.value)} />
 
-            <button type="submit" >sent</button>
+            <button  onClick={handelclick} >sent</button>
         </form>
-        {show?<p>hlo {greet.pname} youre email have been sent succes fully <br /> {detail.name} will contact you soon</p>:<p></p>}
+        {show?<p>hlo {pname} youre email have been sent succes fully <br /> {detail.name} will contact you soon</p>:<p></p>}
         
         
         </div>
