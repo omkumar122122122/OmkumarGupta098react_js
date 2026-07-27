fffb53be9b324d08c4aae00b7ef670402d0ebe49
omkumar122122122 <omkumar122122122@gmail.com>
2026-07-27 14:30:09 +0530

    changes

---
+++ Omkumargupta2024BTCS098/src/features/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+    const detail={
+        name: "Om Kumar Gupta",
+        tag: "Software Developer",      
+        shortBio: "i am a enthuzatic developer can full fill all",  
+        fullBio: "...",          
+        skills: [    
+            { title: "...", items: ["...", "..."] },  
+        ],
+        projects: [    
+            { title: "...", bio: "...", tags: ["...", "..."] },  
+        ],
+    }
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Home
